f0ba472286afa113c5412410991a40890800cdbc
haniyeh <haniye.habibi@gmail.com>
2022-08-15 16:22:17 +0430

    rename command from closepoll to closeissue

--- CustomCommand/wsgi.py
+++ CustomCommand/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TestManage project.
+WSGI config for CustomCommand project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestManage.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CustomCommand.settings')
 
 application = get_wsgi_application()
